07eba89a09c005a28ab34620531705aaab988ed0
kms0207 <75982005+kms0207@users.noreply.github.com>
2023-08-05 18:09:53 +0400

    Fixed Microcontroller

--- Assets/Scripts/Microcontroller/RSMAMicrocontroller.cs
+++ Assets/Scripts/Microcontroller/RSMAMicrocontroller.cs
@@ -3,36 +3,11 @@ using UnityEngine;
 
 public class RSMAMicrocontroller : MonoBehaviour
 {
-    public bool isActive;
     [SerializeField] protected RSMAGPIO GPIO;
     [SerializeField] protected RSMADataTransferMaster dataBus;
     protected void Start()
     {
-        if(isActive)
-<<<<<<< Updated upstream
-            StartCoroutine(MicroStart()); ;
-    }
-    protected void Update()
-    {
-        if(!isLoopEnd && isLoop)
-            StartCoroutine(MicroLoop());
-    }
-    protected IEnumerator MicroStart()
-    {
-        yield return new WaitForSeconds(.1f);
         StartCoroutine(MicroLoop());
-        isLoop = true;
-    }
-    protected IEnumerator MicroLoop()
-    {
-        //TODO убрать
-        Debug.Log("TractorMicro B");
-
-        yield return new WaitForSeconds(.1f);
-        isLoopEnd = false;
-=======
-            StartCoroutine(MicroLoop()); ;
->>>>>>> Stashed changes
     }
 
     protected virtual IEnumerator MicroLoop()
