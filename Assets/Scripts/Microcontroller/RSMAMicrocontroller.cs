2d7158a44e7ebbe4f647ac3d14942cb808bf7ef7
kms0207 <75982005+kms0207@users.noreply.github.com>
2023-08-17 08:37:08 +0700

    Updated microcontroller for new GPIO

--- Assets/Scripts/Microcontroller/RSMAMicrocontroller.cs
+++ Assets/Scripts/Microcontroller/RSMAMicrocontroller.cs
@@ -6,19 +6,26 @@ public abstract class RSMAMicrocontroller : MonoBehaviour
     [SerializeField] protected RSMAGPIO GPIO;
     [SerializeField] protected RSMADataTransferMaster dataBus;
     [SerializeField] protected IMicrocontollerProgramm programm;
+    protected IEnumerator programmCoroutine;
 
-    protected void OnEnable()
+    protected virtual void OnEnable()
     {
         programm = gameObject.GetComponent<IMicrocontollerProgramm>();
         if (programm != null)
         {
             programm.GPIO = this.GPIO;
             programm.dataBus = this.dataBus;
-            StartCoroutine(programm.MainProgramm());
+            programmCoroutine = programm.MainProgramm();
+            StartCoroutine(programmCoroutine);
         }
         else
         {
             Debug.LogError($"Microcontroller {gameObject.name} can't find programm file");
         }
+    }
+    protected virtual void OnDisable()
+    {
+        StopCoroutine(programmCoroutine);
+        GPIO.ResetAll();
     }
 }
