78eaafe81f9c471b8d74666d00cd1fd3fd00e95a
kms0207 <75982005+kms0207@users.noreply.github.com>
2024-03-09 02:43:36 +0400

    #Drone controlls added

--- Assets/Scripts/UtilitiesForUnity/FirstApprox/RSMADrone.cs
+++ Assets/Scripts/UtilitiesForUnity/FirstApprox/RSMADrone.cs
@@ -8,17 +8,34 @@ public class RSMADrone : MonoBehaviour
 
     [SerializeField] private float _mass = 1f;
 
+    private Rigidbody _rigidbody;
+
     public Vector3 targetDirection;
 
     public Vector3 acceleration = Vector3.zero;
 
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
     private void Update()
     {
-        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(acceleration), 0.01f);
+        float pitch = Mathf.Atan2(targetDirection.z, targetDirection.y + _gravity);
+        float yaw = Mathf.Atan2(targetDirection.x, targetDirection.y + _gravity);
+        transform.rotation = Quaternion.Euler(pitch * Mathf.Rad2Deg, 0, yaw * Mathf.Rad2Deg);
+
+        acceleration = transform.up * (_mass * targetDirection.z / Mathf.Sin(pitch));
+
+        if(acceleration.y is float.NaN)
+        {
+            acceleration.y = _gravity;
+        }
     }
 
     private void FixedUpdate()
     {
-        acceleration = targetDirection + (_mass * _gravity * Vector3.down);
+        acceleration = _mass * (targetDirection + (_gravity * Vector3.down));
+        _rigidbody.AddForce(acceleration);
     }
 }
