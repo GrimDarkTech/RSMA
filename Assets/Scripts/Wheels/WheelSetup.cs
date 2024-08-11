e40284d0ad1144b73abf6b9b91eb5dabd2563885
kms0207 <75982005+kms0207@users.noreply.github.com>
2024-08-11 07:05:02 +0400

    #Wheel refactored

--- Assets/Scripts/Wheels/WheelSetup.cs
+++ Assets/Scripts/Wheels/WheelSetup.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+
 /// <summary>
 /// Class for creating and configuring omnidirectional wheels
 /// </summary>
